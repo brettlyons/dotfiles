35598d63e57629af16868dc447a26365acb30940
Brett Lyons <blyons@fastmail.com>
2024-08-18 11:21:58 -0600

    Update dotfiles/install.sh

--- dotfiles/executable_install.sh
+++ dotfiles/executable_install.sh
@@ -1,33 +1,34 @@
 #/bin/bash
 # Meant to be run from within the ~/dotfiles folder
-# assumes uBlue-os or Aurora
+# assumes presence of git, flathub, flatpak and brew
 
 declare -a fonts=("Iosevka.tar.xz" "IosevkaTerm.tar.xz")
 
 for font in "${fonts[@]}"; do
-	if [[ ! -f $font ]]; then
-		curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font
-	fi
-	tar -xvf $font -C ~/.local/share/fonts &
+  if [[ ! -f $font ]]; then
+    curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font
+  fi
+  tar -xvf $font -C ~/.local/share/fonts &
 done
 
 # Install wezterm
 flatpak install flathub org.wezfurlong.wezterm &
 
 if [[ -e Brewfile ]]; then
-	brew bundle install &
+  brew bundle install &
+  chezmoi init --apply brettlyons
 fi
 
 if [[ -e ../.config/nvim/ ]]; then
-	# move neovim config out of the way
-	mv ~/.config/nvim{,.bak}
+  # move neovim config out of the way
+  mv ~/.config/nvim{,.bak}
 
-	# Move neovim stuff out of the way
-	mv ~/.local/share/nvim{,.bak}
-	mv ~/.local/state/nvim{,.bak}
-	mv ~/.cache/nvim{,.bak}
-	# clone
-	git clone https://github.com/LazyVim/starter ~/.config/nvim &
-	# fetch lazyvim packages
-	nvim --headless -c q!
+  # Move neovim stuff out of the way
+  mv ~/.local/share/nvim{,.bak}
+  mv ~/.local/state/nvim{,.bak}
+  mv ~/.cache/nvim{,.bak}
+  # clone
+  git clone https://github.com/LazyVim/starter ~/.config/nvim &
+  # fetch lazyvim packages
+  nvim --headless -c q!
 fi
